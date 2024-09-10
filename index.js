e47ba97b767ea083f0257a7115b0218dc7eca37e
Developer Creations <141968699+melvinCodex@users.noreply.github.com>
2024-09-10 19:13:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+// constants variables
+
+const name = 859;
+
+var radius = 50;
+
+const pie = 3.142;
+
+const areaOfCIrcle = pie*radius*radius;
+
+console.log(areaOfCIrcle);
+
+//primitive types {data types}
+//number string boolean null/undefined 
+//arrays, object, document ,json
+var isEqual = null;
+
+let number1 = 5300;
+
+let number2 = 50;
+
+bigger = `this is the number${number1}`;
+let myName = 'melvin';
+let secondName = '"Kinoti"';
+let thirdName = `melvin`;
+
+let fullName = myName+ '    ' + secondName;
+
+//Arithmetic operaters
+// plus + Multiply * Division / minus - modulus % 
+let b = 9;
+let h = 20;
+let s = 20;
+let a = 10;
+
+//logical operators
+// OR AND  
+//   || or        &&  and    !  not
+if (!(a == b)) {
+    alert("Evaluated true ,  Is equal");
+} else {
+    alert("Evaluated false ,  Is not equal");
+}
+const areaOfTriangle = 0.5*b*h;
+
+const areaOfSquare = s*s;
+
+const areaOfTrapezium = 0.5*(a + b)*h;
+
+const modulus = a % b;
+
+const bool = h > s;
+
+console.log(bool);
+
+
+//Comparison operaters
+// greater than ,Less than ,equal to, if
+// == === > <
+
+if (a == b) {
+    //alert("Equal");
+} else {
+    //alert("Not equal");
+}
+
